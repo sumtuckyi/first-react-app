7e93703f51754461e7c1eeb1b30952359f02c874
sumtuckyi <sumin510@naver.com>
2023-02-12 05:39:16 +0900

    css

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -9,7 +9,10 @@ function nav () {
                     <Link to={`${process.env.PUBLIC_URL}/`}><li className={styles.option}>Home</li></Link>
                     <Link to={`/tv_series`}><li className={styles.option}>TV Shows</li></Link>
                     <Link to={`/recent`}><li className={styles.option}>Recent</li></Link>
+                    <input className={styles.input} placeholder="search"></input>
+                    <button className={styles.button}><img src="./1.png"/>go</button>
                 </ul>
+    
             </nav>
         </div>
     );
